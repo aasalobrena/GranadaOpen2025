bc570990b4e43cb447bee81437bf10f77f9ffa57
aasalobrena <alvaroaasgm@gmail.com>
2025-03-27 17:43:45 +0100

    assign-skewb-awards

---
+++ 12-assign-skewb-awards.cs
@@ -0,0 +1,4 @@
+# 30 is other-awards-skewb
+ManuallyAssignActivity(28, Persons((RoundPosition(_skewb-r1) == 1)), "staff-Winner")
+ManuallyAssignActivity(28, Persons((RoundPosition(_skewb-r1) == 2)), "staff-RunnerUp")
+ManuallyAssignActivity(28, Persons((RoundPosition(_skewb-r1) == 3)), "staff-Third")
