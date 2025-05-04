aa2cdc05f6ceccb77fe1d65400a9f6bedd4c31be
aasalobrena <alvaroaasgm@gmail.com>
2025-05-04 23:55:06 +0200

    :(

--- 03-assign-r1.cs
+++ 03-assign-r1.cs
@@ -4,7 +4,7 @@
 ManuallyAssignActivity(1, Persons(true))
 
 # 2 is other-tutorial
-ManuallyAssignActivity(2, Persons(And(IsNull(WcaId())), (Country() == "ES")), "staff-Alumno")
+ManuallyAssignActivity(2, Persons(IsNull(WcaId())), "staff-Alumno")
 ManuallyAssignActivity(2, [2016GALA04, 2015SANC18])
 ManuallyAssignActivity(2, [2018CAST11, 2010GARC02], "staff-judge")
 
